5c50e4740e04163e0e5a37a4152fb304731cdde7
nayanika2304 <nayanikabhargava1993@gmail.com>
2020-08-24 09:33:54 -0400

    error handling and dialog

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -51,6 +51,10 @@ router.post(
           id: createdPost._id
         }
       });
+    }).catch(error =>{
+      res.status(500).json({
+        message: 'Creating a post failed!'
+      })
     });
   }
 );
@@ -79,6 +83,10 @@ router.put(
         res.status(401).json({message:"Not authorized"})
       }
 
+    }).catch(error =>{
+      res.status(500).json({
+        message: 'Couldn`t update post'
+      })
     });
   }
 );
@@ -102,7 +110,11 @@ router.get("", (req, res, next) => {
         posts: fetchedPosts,
         maxPosts: count
       });
-    });
+    }).catch(error =>{
+      res.status(500).json({
+        message:'Fetching posts failed!'
+      })
+  });
 });
 
 router.get("/:id", (req, res, next) => {
@@ -112,6 +124,10 @@ router.get("/:id", (req, res, next) => {
     } else {
       res.status(404).json({ message: "Post not found!" });
     }
+  }).catch(error =>{
+    res.status(500).json({
+      message:'Fetching posts failed!'
+    })
   });
 });
 
@@ -122,7 +138,10 @@ router.delete("/:id", checkAuth, (req, res, next) => {
     }else{
       res.status(401).json({message:"Not authorized"})
     }
-
+  }).catch(error =>{
+    res.status(500).json({
+      message:'Fetching posts failed!'
+    })
   });
 });
 
